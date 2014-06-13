5575b59bd81a08a93493ab7e2f76c9dff26b74d8
Rafael Campos Nunes <rafaelnunes737@hotmail.com>
2014-06-13 06:01:30 -0300

    Changed a little of informations.

--- src/conf.lua
+++ src/conf.lua
@@ -8,7 +8,7 @@ function love.conf(t)
     t.release = false
     t.screen.width = 800
     t.screen.height = 600
-    t.screen.fullscreen = true
+    t.screen.fullscreen = false
     t.screen.vsync = true
     t.screen.fsaa = 0           -- The number of FSAA-buffers (number)
     t.modules.joystick = false   -- Enable the joystick module (boolean)
